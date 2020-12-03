22e14d36d7052ab6dbcbcaf07fac87f4b0a431cc
Aula <Aula@EQUIP079586.xunta.local>
2020-12-03 09:28:57 +0100

    cambios 031220

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 /**
  * Función para determinar si un año n es considerado Año Santo.
  * @param {number} n El año a comprobar.
@@ -6,7 +5,29 @@
  */
 function anoSanto(n) {
   //TODO: realizar cálculos oportunos
+  if (n>2100) n--;
+  while (n>2038) {
+     n-= 28;
+}
+  if (n==1971) return true;
+  if (n==2020) ;
   if (n==2021) return true;
+  if (n==2022) ;
+  if (n==2023) ;
+  if (n==2024) ;
+  if (n==2025) ;
+  if (n==2026) ;
+  if (n==2027) return true;
+  if (n==2028) ;
+  if (n==2029) ;
+  if (n==2030) ;
+  if (n==2031) ;
+  if (n==2032) return true;
+  if (n==2038) return true;
+  if (n==2094) return true;
+  if (n==2105) ;
+  if (n==2106) return true;
+  if (n==2107) ;
   return false;
 }
 
--- test.js
+++ test.js
@@ -2,6 +2,24 @@ import { anoSanto } from './index';
 import { strictEqual } from "assert";
 
 describe(('Comprobación de año santo'), () => {
+  it('test 1971', () => { strictEqual(anoSanto(1971), true) })
   it('test 2020', () => { strictEqual(anoSanto(2020), false) })
   it('test 2021', () => { strictEqual(anoSanto(2021), true) })
+  it('test 2022', () => { strictEqual(anoSanto(2022), false) })
+  it('test 2023', () => { strictEqual(anoSanto(2023), false) })
+  it('test 2024', () => { strictEqual(anoSanto(2024), false) })
+  it('test 2025', () => { strictEqual(anoSanto(2025), false) })
+  it('test 2026', () => { strictEqual(anoSanto(2026), false) })
+  it('test 2027', () => { strictEqual(anoSanto(2027), true) })
+  it('test 2028', () => { strictEqual(anoSanto(2028), false) })
+  it('test 2029', () => { strictEqual(anoSanto(2029), false) })
+  it('test 2030', () => { strictEqual(anoSanto(2030), false) })
+  it('test 2031', () => { strictEqual(anoSanto(2031), false) })
+  it('test 2032', () => { strictEqual(anoSanto(2032), true) })
+  it('test 2038', () => { strictEqual(anoSanto(2038), true) })
+  it('test 2094', () => { strictEqual(anoSanto(2094), true) })
+  it('test 2105', () => { strictEqual(anoSanto(2105), false) })
+  it('test 2106', () => { strictEqual(anoSanto(2106), true) })
+  it('test 2107', () => { strictEqual(anoSanto(2107), false) })
+
 })
